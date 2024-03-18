ab57435a3106d2bc219ee1c14d904d84818e0847
Shimizu Izumi <shimizuizumi@gmx.net>
2024-03-18 17:47:34 +0100

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,47 @@
-import Link from "next/link";
+"use client";
+
+import { Input } from "@nextui-org/input";
+import { Snippet } from "@nextui-org/snippet";
+import { useState } from "react";
+import { Select, SelectItem } from "@nextui-org/select";
+import owoify from "owoify-js";
 
 export default function HomePage() {
+  const [text, setText] = useState("hello! i am your worst nightmare");
+  const [variant, setVariant] = useState("owo");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <h1 className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-5xl font-extrabold tracking-tight text-transparent sm:text-[5rem]">
+        OwOfier
+      </h1>
+      <div className="flex w-unit-96 flex-col flex-wrap gap-4 sm:flex-1 md:gap-8">
+        <Input
+          type="text"
+          label="Input"
+          onChange={(e) => setText(e.target.value)}
+          placeholder={"hello! i am your worst nightmare"}
+        />
+        <Select
+          label="Mode"
+          defaultSelectedKeys={["owo"]}
+          onChange={(e) => setVariant(e.target.value)}
+          disallowEmptySelection
+        >
+          <SelectItem key="owo" value="owo">
+            OwO
+          </SelectItem>
+          <SelectItem key="uwu" value="uwu">
+            UwU
+          </SelectItem>
+          <SelectItem key="uvu" value="uvu">
+            UvU
+          </SelectItem>
+        </Select>
+        <Snippet symbol={""} className="">
+          {owoify(text, variant)}
+        </Snippet>
       </div>
-    </main>
+    </div>
   );
 }
